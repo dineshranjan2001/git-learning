4bd00073ead1fdca80403148cdd90a541d87fca2
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2024-01-12 11:55:47 +0530

    Create PallindromeNumber.java
    Code in main method code the basic changes

---
+++ PallindromeNumber.java
@@ -0,0 +1,8 @@
+public class PallindromeNumber{
+  public static void main(String args[]){
+      int number=121;
+      boolean checkPalindrome=checkPallindrome(number);
+      System.out.println("The given number is pallindrome "+checkPalindrome);
+  }
+  
+}
